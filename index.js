b438e3fab55371774f5f9249b30da3dc3428335c
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2021-11-19 15:42:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b) {
+    let sum = a + b
+    return sum
+  }
+  
+
+function isEven(number) {
+    const inputedNumber = Number(prompt(`please enter a number betwen 1-100`)) 
+    const itsEven = inputedNumber
+        if(inputedNumber% 2 === 0) return true
+        return false 
+}
+
+function greet() {
+    let name = prompt(`Please enter yor name`)
+    alert (`welcome ${name}`)
+  }
+  
+function isAnAdult() {
+    let age = Number(prompt(`enter yor age`))
+        if(age < 18) {
+        let year = 2021
+        let bornYear = year- age
+        alert(` please come back in ${18 - (year - bornYear)} years`)}
+            else 
+        alert (`welcome`)
+        return true
+  }
+  
+  // Input: age (number)
+  // Action: Check how many years are left until adulthood (18)
+  // Output: The result (number)
+function yearsToAdulthood() {
+    // write your code here
+  }
+  
+  // Input: person ({ age: number, name: string })
+  // Action:
+  //   - check if a person is an adult
+  //   - if they are, greet them
+  //   - if they are not, tell them to come back in X years (when they are)
+  // Output: The result (string)
+function admit() {
+    // write your code here
+    // use greet, isAnAdult and yearsToAdulthood to help you!
+  }
+  
